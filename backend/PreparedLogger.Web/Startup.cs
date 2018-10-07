487c4ab6eded6d630615c32f6fd6c7c998f81d2f
Jeremy Rann <xerog1@gmail.com>
2018-10-07 17:40:01 -0400

    Swagger integration

--- backend/PreparedLogger.Web/Startup.cs
+++ backend/PreparedLogger.Web/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PreparedLogger.DataAccess;
 using PreparedLogger.DataAccess.SqlServer;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PreparedLogger.Web
 {
@@ -42,6 +43,10 @@ namespace PreparedLogger.Web
             }
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Prepared Logger API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +64,12 @@ namespace PreparedLogger.Web
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prepared Logger API v1");
+            });
             app.UseMvc();
         }
     }
